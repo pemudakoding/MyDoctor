2a0c20cd95384eed0ff6eadc60032406c15b1a56
pemudakoding <pemudakoding@gmail.com>
2021-08-31 21:13:21 +0800

    modified: register page used state

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TextInput, View} from 'react-native';
 import {colors, fonts} from '../../../utils';
 
-export default function Input({label}) {
+export default function Input({label, value, onChangeText, secureTextEntry}) {
+  const [border, setBorder] = useState(colors.border);
+  const onFocusForm = () => {
+    setBorder(colors.tertiary);
+  };
+
+  const onBlurForm = () => {
+    setBorder(colors.border);
+  };
   return (
     <View>
       <Text style={styles.label}>{label}</Text>
-      <TextInput style={styles.input} />
+      <TextInput
+        onFocus={onFocusForm}
+        onBlur={onBlurForm}
+        style={styles.input(border)}
+        value={value}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  input: {
+  input: (border) => ({
     borderWidth: 1,
-    borderColor: colors.border,
+    borderColor: border,
     borderRadius: 10,
     padding: 12,
-  },
+  }),
   label: {
     fontSize: 16,
     color: colors.text.secondary,
